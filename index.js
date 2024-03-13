f7895039b25d85125eb7e508056b5701e608d7cb
Fensley <fortilusnehemie@gmail.com>
2024-03-13 13:34:31 -0400

    final initial

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
- console.log(window.innerHeight);
- console.log(123)
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll" , evenfunc)
+
+evenfunc();
+
+function evenfunc(){
+  const heightW =  window.innerHeight / 5 * 4
+
+  boxes.forEach(box => {
+    const boxtop = box.getBoundingClientRect().top;
+    if (boxtop < heightW){
+        box.classList.add('show')
+    } else{
+        box.classList.remove('show')
+    }
+  })
+  
+}
